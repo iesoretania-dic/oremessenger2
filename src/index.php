ca52770f59834ee62ae84a84ad4a7e80ca67cbf2
Luis Ramón López <lrlopez@gmail.com>
2023-10-25 14:23:34 +0200

    Added login form

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>OreMessenger 2</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form method="post">
+        <input type="text" name="username" placeholder="Enter your username" />
+        <input type="password" name="pass" placeholder="Enter password" />
+        <button type="submit" name="login">Log in</button>
+    </form>
+</body>
+</html>

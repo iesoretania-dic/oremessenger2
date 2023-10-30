9a908bd91913871a0ad2fa33c635f9bd30112a24
Luis Ramón López <lrlopez@gmail.com>
2023-10-30 09:13:22 +0100

    Show user messages

--- src/messages.php
+++ src/messages.php
@@ -14,5 +14,31 @@ $username = $_SESSION['username'];
 </head>
 <body>
     <h1>Browse messages - <?= htmlentities($username) ?></h1>
+    <table border="1px">
+        <thead>
+        <tr>
+            <th>Username</th>
+            <th>Message body</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $db = new PDO('mysql:host=127.0.0.1;port=33060;dbname=oremessenger',
+            'root', 'ejemplo_pass');
+
+        $messages = $db->prepare('SELECT * FROM message WHERE username=:user');
+        $messages->bindValue(':user', $username);
+        $messages->setFetchMode(PDO::FETCH_ASSOC);
+        $messages->execute();
+
+        foreach ($messages as $message) {
+            echo "<tr>";
+            echo "<td>" . htmlentities($message['username']) . "</td>";
+            echo "<td>" . htmlentities($message['body']) . "</td>";
+            echo "</tr>";
+        }
+        ?>
+        </tbody>
+    </table>
 </body>
 </html>
